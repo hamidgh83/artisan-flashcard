617a7496f77aaac41fe6328c7387fbd20bdb65e7
Hamid Ghorashi <h.ghorashi@gmail.com>
2022-08-27 13:38:37 +0400

    Create migration for practice table.

---
+++ database/migrations/2022_08_25_193741_create_practices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('practices', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('flash_card_id');
+            $table->enum('result', ['NOT ANSWERED', 'CORRECT', 'INCORRECT'])->index();
+            $table->string('answer');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('flash_card_id')->references('id')->on('flash_cards');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('practices');
+    }
+};
